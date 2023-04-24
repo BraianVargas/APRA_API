a96aabcf48c442959153a390f148a891826c7071
Braian Vargas <braianvargas1616@gmail.com>
2023-04-24 19:56:05 -0300

    [RENAME] Json Files

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, jsonify
-from controllers. mainController import *
+from controllers.mainController import *
+from controllers.etlController import *
 
 app = Flask(__name__)
 app.config.from_pyfile("./data/config.py")
 
 @app.route('/historical')
 def getHistorical():
-    return proccessData("historical")
+    return proccessData("historicos")
 
 @app.route('/monthly')
 def getMonthly():
-    return proccessData("monthly")
+    return proccessData("mensuales")
 
 @app.route('/etl')
 def loadEtl():
--- controllers/mainController.py
+++ controllers/mainController.py
@@ -4,13 +4,12 @@ from flask import jsonify, current_app, g
 
 def getDB():
     if 'db' not in g:
-        g.db=mysql.connector.connect( 
+        g.db = mysql.connector.connect( 
             host=current_app.config['DATABASE_HOST'],
             user=current_app.config['DATABASE_USER'],
             password=current_app.config['DATABASE_PASSWORD'],
             database=current_app.config['DATABASE']
         )
-        
         g.c = g.db.cursor(dictionary=True)
     return g.db, g.c
 
@@ -18,39 +17,24 @@ def getDB():
 
 def proccessData(type):
     db, g= getDB()
-    # Crear un cursor para ejecutar las consultas
+
     mycursor = db.cursor()
     
-    # Definir la consulta SQL
-    if type== "historical":
-        sql = "SELECT * FROM datos_historicos"
-    else:
-        sql = "SELECT * FROM datos_mensuales"
+    sql = f"SELECT * FROM datos_{str(type)}"
+
     mycursor.execute(sql)
     result= mycursor.fetchall()
 
-    # Obtener los nombres de las columnas
     column_names = [desc[0] for desc in mycursor.description]
 
-    # Crear una lista para almacenar los objetos JSON de cada fila de datos
     json_list = []
 
-    # Leer el archivo JSON
-    with open(f"./data/glosary_{type}.json", "r") as file:
+    with open(f"./data/glosario_{type}.json", "r") as file:
         data = json.load(file)
 
-    # Iterar sobre los resultados y agregar cada fila de datos a la lista
     for row in result:
-        # Crear un diccionario para almacenar los datos de esta fila
         data_dict = {}
-        # Iterar sobre las columnas y agregar los valores al diccionario
         for i, col_name in enumerate(column_names):
             data_dict[data[col_name]] = row[i]
-        # Agregar el diccionario a la lista de objetos JSON
         json_list.append(data_dict)
-    # Devolver la lista de objetos JSON en formato JSON
     return jsonify(json_list)
-
-
-def loadDatabase():
-    pass
